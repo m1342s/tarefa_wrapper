690307a7c6d34e9b5f3ca6a687fa8845d15a65c7
Matheus Viersa <matheusviersa995@hotmail.com>
2025-03-27 13:24:53 -0300

    Tarefa corrigida após apontamentos

--- src/TarefaWrapper.java
+++ src/TarefaWrapper.java
@@ -2,5 +2,7 @@ public class TarefaWrapper {
     public static void main(String[] args) {
         Integer variavel =1000;
         System.out.println(variavel);
+        Integer variavel2 = variavel;
+        System.out.println(variavel2);
     }
 }
