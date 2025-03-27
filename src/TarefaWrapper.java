d40841fa7bf23d58b57a0d7b1daef68c83c17793
Matheus Viersa <matheusviersa995@hotmail.com>
2025-03-27 12:21:52 -0300

    Tarefa finalizada

---
+++ src/TarefaWrapper.java
@@ -0,0 +1,8 @@
+public class TarefaWrapper {
+    public static void main(String[] args) {
+         int valor=100;
+         System.out.println(valor);
+         Integer valor2=100;
+         System.out.println(valor2);
+    }
+}
